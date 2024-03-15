3de2db4f646c7ec9c2bc01282a5d2dc5b2ee7870
Rasul Aslanov <rasulsha@code.edu.az>
2024-03-16 02:08:09 +0400

    First

---
+++ ChatAppTCP/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatAppTCP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IPAddress ip =IPAddress.Parse("127.0.0.1");
+        public int port = 27001;
+        public IPEndPoint ServerEp {  get; set; }
+       public TcpClient tcpClient {  get; set; }
+        public NetworkStream? clientdata { get; set; }
+        public BinaryReader? reader { get; set; }
+        public BinaryWriter? writer { get; set; }
+
+        public List<string> AllUser=new List<string>();
+        public string _name {  get; set; }
+        public MainWindow(string name)
+        {
+            _name=name;
+            ServerEp=new IPEndPoint(ip, port);
+            tcpClient = new TcpClient();
+            tcpClient.Connect(ServerEp);
+            
+            clientdata= tcpClient.GetStream();
+            reader = new BinaryReader(clientdata);
+            writer = new BinaryWriter(clientdata);
+            writer.Write(name);
+            Task.Run(() => { while (true) { writer.Write("online"); } });
+            InitializeComponent();
+        }
+        
+        
+
+
+    }
+}
---
+++ ServerSide/Program.cs
@@ -0,0 +1,73 @@
+﻿
+
+using System.Net;
+using System.Net.Sockets;
+
+IPAddress ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+
+var listenerEP= new IPEndPoint(ip, port);
+
+TcpListener listener = new TcpListener(listenerEP);
+
+listener.Start();
+Console.WriteLine($"Server is running on {listener.LocalEndpoint}");
+
+Dictionary<TcpClient ,string> allclients= new Dictionary<TcpClient ,string>();
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    Console.WriteLine($"Client Connected {client.Client.RemoteEndPoint}");
+    NetworkStream? clientdata = client.GetStream();
+    BinaryReader? reader = new BinaryReader(clientdata);
+    var name = reader.ReadString();
+    allclients.Add(client, name);
+    
+    //handshake
+    _ = Task.Run(() =>
+    {
+        NetworkStream? clientdata = client.GetStream();
+        BinaryReader? reader = new BinaryReader(clientdata);
+        BinaryWriter? writer = new BinaryWriter(clientdata);
+        while (true)
+        {
+            try
+            {
+                while (true)
+                {
+                    reader.ReadString();
+                }
+
+            }
+            catch (Exception)
+            {
+
+               allclients.Remove(client);
+            }
+            
+        }
+
+    });
+
+    _ = Task.Run(() =>
+    {
+
+    });
+
+    _ = Task.Run(() =>
+    {
+        NetworkStream? clientdata=client.GetStream();
+        BinaryReader? reader=new BinaryReader(clientdata);
+        BinaryWriter? writer=new BinaryWriter(clientdata);
+
+        while (true)
+        {
+        var  message = reader.ReadString();
+        Console.WriteLine($"Client {message}");
+        }
+
+    });
+
+}
+
